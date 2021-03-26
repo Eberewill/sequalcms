63bcf0626722524b2e7d17d1e004e1f9069c723f
eberewill <eberewill@gmail.com>
2021-03-26 19:33:12 +0100

    working deployment on GCP

--- config/database.js
+++ config/database.js
@@ -12,6 +12,7 @@ const { Sequelize } = require("sequelize");
     idle: 10000,
   },
 });*/
+/*
 var db = new Sequelize("awsdb", "conwilliams", "KTxYTugQbu0n0VYQsel7", {
   host: "database-1.cgq5cwzjj2ki.us-east-1.rds.amazonaws.com",
   port: 5432,
@@ -26,6 +27,26 @@ var db = new Sequelize("awsdb", "conwilliams", "KTxYTugQbu0n0VYQsel7", {
   },
   pool: { maxConnections: 5, maxIdleTime: 30 },
   language: "en",
-});
+});*/
+var db = new Sequelize(
+  "dalr4rll8oap0f",
+  "cwikseysdctypg",
+  "30dde61070a875df048d6ba9909cdd5436d92313f7197f9e0f0769653931476d",
+  {
+    host: "ec2-52-71-161-140.compute-1.amazonaws.com",
+    port: 5432,
+    logging: console.log,
+    maxConcurrentQueries: 100,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+    pool: { maxConnections: 5, maxIdleTime: 30 },
+    language: "en",
+  }
+);
 
 module.exports = db;
