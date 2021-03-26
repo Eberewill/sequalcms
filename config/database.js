e5ffa3c0892ade84655f232ade0bbbaf93175274
eberewill <eberewill@gmail.com>
2021-03-26 14:42:56 +0100

    migratedb

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
 const { Sequelize } = require("sequelize");
-const db = new Sequelize("willsdb", "postgres", "eberechukwu", {
+
+/*const db = new Sequelize("willsdb", "postgres", "eberechukwu", {
   host: "localhost",
   dialect: "postgres",
   port: 5200,
@@ -10,6 +11,23 @@ const db = new Sequelize("willsdb", "postgres", "eberechukwu", {
     acquire: 30000,
     idle: 10000,
   },
-});
+});*/
+var db = new Sequelize(
+  "willsprodweb.cgq5cwzjj2ki.us-east-1.rds.amazonaws.com",
+  "postgres",
+  "KTxYTugQbu0n0VYQsel7",
+  {
+    host: "****.****.us-west-1.rds.amazonaws.com",
+    port: 5432,
+    logging: console.log,
+    maxConcurrentQueries: 100,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: "Amazon RDS",
+    },
+    pool: { maxConnections: 5, maxIdleTime: 30 },
+    language: "en",
+  }
+);
 
 module.exports = db;
