116e5973081481afe1a9ab4b0d2f30fa491ff3be
eberewill <eberewill@gmail.com>
2021-03-26 14:53:01 +0100

    deployment

--- config/database.js
+++ config/database.js
@@ -12,22 +12,17 @@ const { Sequelize } = require("sequelize");
     idle: 10000,
   },
 });*/
-var db = new Sequelize(
-  "willsprodweb.cgq5cwzjj2ki.us-east-1.rds.amazonaws.com",
-  "postgres",
-  "KTxYTugQbu0n0VYQsel7",
-  {
-    host: "****.****.us-west-1.rds.amazonaws.com",
-    port: 5432,
-    logging: console.log,
-    maxConcurrentQueries: 100,
-    dialect: "postgres",
-    dialectOptions: {
-      ssl: "Amazon RDS",
-    },
-    pool: { maxConnections: 5, maxIdleTime: 30 },
-    language: "en",
-  }
-);
+var db = new Sequelize("willsprodweb", "postgres", "KTxYTugQbu0n0VYQsel7", {
+  host: "willsprodweb.cgq5cwzjj2ki.us-east-1.rds.amazonaws.com",
+  port: 5432,
+  logging: console.log,
+  maxConcurrentQueries: 100,
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: "Amazon RDS",
+  },
+  pool: { maxConnections: 5, maxIdleTime: 30 },
+  language: "en",
+});
 
 module.exports = db;
