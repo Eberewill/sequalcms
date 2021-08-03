87e46bfd7ee5d5d7c837c1309f965cc0ae2775aa
Eberechi <eberewill@gmail.com>
2021-08-03 21:14:30 +0100

    removed firebase keys

--- services/firebase-client.js
+++ services/firebase-client.js
@@ -1,12 +1,12 @@
 const firebase = require("firebase");
 
 firebase.initializeApp({
-  apiKey: "AIzaSyCOkrcvQvTBf50-YbY5ALSFKvwIyWwI_J4",
-  authDomain: "schoolverse.firebaseapp.com",
-  databaseURL: "https://schoolverse.firebaseio.com",
-  projectId: "schoolverse",
-  storageBucket: "schoolverse.appspot.com",
-  messagingSenderId: "515184959709",
-  appId: "1:515184959709:web:6663376119075b4a3e7b34",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 });
 module.exports = { firebase };
